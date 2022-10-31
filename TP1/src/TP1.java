5f989d8ca7f3d6bd200aa5d723edfec841917ab2
RodrigoPrintes <drigoprintes00@gmail.com>
2022-10-31 10:07:22 -0400

    Classes principais

--- TP1/src/TP1.java
+++ TP1/src/TP1.java
@@ -1,6 +1,6 @@
 public class TP1 {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("TP1 - Escalonamento de Processos!");
     }
 }
